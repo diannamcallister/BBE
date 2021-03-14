7278aa4d7a1b9a18a802a29521ab027b525d38d2
Micaela Consens <micaela.consens@mail.utoronto.ca>
2021-03-13 20:44:46 -0500

    legth to length

--- backend/models/publication.js
+++ backend/models/publication.js
@@ -4,49 +4,49 @@ const Publication = mongoose.model('Publication', {
     doi: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     abstract: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
 	title: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     author: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     journal: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     date: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     concepts: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     summaryByUs: {
 		type: String,
 		required: true,
-		minlegth: 1,
+		minlength: 1,
 		trim: true
     },
     
